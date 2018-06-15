5d4cc3ac5386210297484007aa09e392deab6f73
Icaro Cristofolini <icaroaj@hotmail.com>
2018-06-14 23:09:21 -0300

    Classe Model_Aluno

---
+++ src/Model_Aluno.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Model_Aluno {
+	private String nome;
+	private ArrayList<String> anotacao;
+
+
+	public void aplicarAnotacao() {
+	
+	}
+	
+	public String pegaAnotacao() {
+		
+		return null;		
+	}
+	
+	public String aluno (String nome) {
+		
+		return nome;
+	}
+	
+	public String criaAnotacao(String anotacao) {
+		
+		return anotacao;		
+	}
+}
